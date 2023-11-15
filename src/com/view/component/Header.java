46391c9d84b79d4a8ed880f58fd663c43b24f35c
Tom <buianh120403@gmail.com>
2023-11-15 19:31:32 +0700

    refactor: First commit

---
+++ src/com/view/component/Header.java
@@ -0,0 +1,126 @@
+package com.view.component;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+public class Header extends javax.swing.JPanel {
+
+    public Header() {
+        initComponents();
+        setOpaque(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        PopupMenu = new javax.swing.JPopupMenu();
+        Exit = new javax.swing.JMenuItem();
+        jLabel1 = new javax.swing.JLabel();
+        searchText1 = new com.view.swing.SearchText();
+        jLabel2 = new javax.swing.JLabel();
+
+        PopupMenu.setOpaque(false);
+
+        Exit.setText("Exit");
+        Exit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ExitActionPerformed(evt);
+            }
+        });
+        PopupMenu.add(Exit);
+
+        setBackground(new java.awt.Color(0, 0, 0));
+        setForeground(new java.awt.Color(36, 36, 36));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/search.png"))); // NOI18N
+
+        searchText1.setBackground(new java.awt.Color(22, 23, 23));
+        searchText1.setForeground(new java.awt.Color(255, 255, 255));
+        searchText1.setCaretColor(new java.awt.Color(22, 23, 23));
+        searchText1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchText1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/menu.png"))); // NOI18N
+        jLabel2.setBorder(javax.swing.BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel2MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(27, 27, 27)
+                .addComponent(searchText1, javax.swing.GroupLayout.DEFAULT_SIZE, 585, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(searchText1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void searchText1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchText1ActionPerformed
+        performSearch();
+    }//GEN-LAST:event_searchText1ActionPerformed
+
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
+        System.out.println("Button click ");
+        
+        PopupMenu.show(this, jLabel2.getX() - 10, jLabel2.getY() + 40);
+        
+    }//GEN-LAST:event_jLabel2MouseClicked
+
+    private void ExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ExitActionPerformed
+        // Get the parent JFrame of this component
+        java.awt.Window window = javax.swing.SwingUtilities.getWindowAncestor(this);
+
+        // Check if the parent is a JFrame and close it
+        if (window instanceof java.awt.Frame) {
+            java.awt.Frame frame = (java.awt.Frame) window;
+            frame.dispose(); // Close the frame
+        }
+    }//GEN-LAST:event_ExitActionPerformed
+    
+    private void performSearch() {
+        // Your search logic here
+        // This method will be called when Enter is pressed or the action is performed
+        System.out.println("Performing search... ");
+        System.out.print("Search words: ");
+        System.out.println(searchText1.getText());
+    }
+    
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        g2.fillRect(0, 0, 25, getHeight());
+        g2.fillRect(getWidth() - 25, getHeight() - 25, getWidth(), getHeight());
+        super.paintComponent(grphcs);
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem Exit;
+    private javax.swing.JPopupMenu PopupMenu;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private com.view.swing.SearchText searchText1;
+    // End of variables declaration//GEN-END:variables
+}
