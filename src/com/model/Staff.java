f389383ab1fbb42a0647a9dbbd9cb5bc25cf7595
Tom <buianh120403@gmail.com>
2023-11-30 19:27:23 +0700

    feat: Login

--- src/com/model/Staff.java
+++ src/com/model/Staff.java
@@ -5,6 +5,10 @@ public class Staff {
     private int age;
     private String email;
     private String address;
+    private String username;
+    private String password;
+    private int previlege;
+    
     
     public Staff(int staffId, String name, int age, String email, String address) {
         this.staffId = staffId;
@@ -13,6 +17,54 @@ public class Staff {
         this.email = email;
         this.address = address;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPrevilege() {
+        return previlege;
+    }
+
+    public void setPrevilege(int previlege) {
+        this.previlege = previlege;
+    }
+
+    public Staff(int staffId, String name, int age, String email, String address, String username, String password, int previlege) {
+        this.staffId = staffId;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+        this.username = username;
+        this.password = password;
+        this.previlege = previlege;
+    }
+
+    public Staff() {
+        this.staffId = 0;
+        this.name = "";
+        this.age = 0;
+        this.email = "";
+        this.address = "";
+        this.username = "";
+        this.password = "";
+        this.previlege = 0;
+    }
+    
+    
 
     public int getStaffId() {
         return staffId;
